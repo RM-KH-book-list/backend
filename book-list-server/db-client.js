090274483508724f1d18f6943ad670f6e83faec4
Ryan Manro <rmanro@gmail.com>
2018-03-12 15:34:16 -0700

    added server.js and connecting to server and sql

--- book-list-server/db-client.js
+++ book-list-server/db-client.js
@@ -8,7 +8,7 @@ const Client = pg.Client;
 
 const client = new Client(DATABASE_URL);
 client.connect()
-    .then(() => console.log('connected to db', DATABASE_URL))
+    .then(() => console.log('connected to db'))
     .catch(err => console.error('connection error', err));
 
 client.on('error', err => console.error(err));
