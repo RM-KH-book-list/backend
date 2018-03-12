2e89e94dc4951b22b73d3c181e5703bf3df7dc62
Keli Hansen <keli.p.hansen@gmail.com>
2018-03-12 14:15:37 -0700

    create and set up load-db files

---
+++ book-list-server/load-db/create-tables.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const client = require('../db-client');
+
+client.query(`
+
+
+`)
+    .then(
+        () => console.log('db task successful'),
+        err => console.error(err)
+    )
+    .then(() => client.end());
